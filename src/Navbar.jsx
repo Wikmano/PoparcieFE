c20ec47902bf68e4fe93a2b2425ed49a72717e40
Wikmano <wiktor.langowski@gmail.com>
2026-04-16 18:45:05 +0200

    Navbar update barwy

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,23 +1,25 @@
+import godlo from './assets/godlo.png';
 import './Navbar.css';
 
 function Navbar() {
   return (
-   <nav className="navbar">
-     <div className='navbar-logo'>
-        <div className="logo-title">
-            <span className="logo-m">M</span>Poparcie
+    <nav className="navbar">
+      <div className='navbar-logo'>
+        <img src={godlo} alt="Godło" className="navbar-godlo" />
+        <div className="navbar-brand">
+          <span className="logo-name"><span className="logo-m">m</span>Poparcie</span>
+          <span className="logo-slogan">Głosujesz i decydujesz</span>
         </div>
-       <span>Głosujesz i decydujesz</span>
-       </div>
-  
+      </div>
+
       <ul className="navbar-links">
-         <li><a href="#Registration">Rejestracja</a></li>
-         <li><a href="#login">Logowanie</a></li>
-         <li><a href="#Organizations">Organizacje</a></li>
-         <li><a href="#about">O projekcie</a></li>
-       </ul>
+        <li><a href="#Registration" className="nav-btn-secondary">Rejestracja</a></li>
+        <li><a href="#login" className="nav-btn-primary">Logowanie</a></li>
+        <li><a href="#Organizations">Organizacje</a></li>
+        <li><a href="#about">O projekcie</a></li>
+      </ul>
     </nav>
-    );
- }
-  
- export default Navbar;
+  );
+}
+
+export default Navbar;
