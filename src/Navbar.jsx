82ffcde96a630b5dec5f967fe13ada8ff1548145
wojtek1866s <279486@student.pwr.edu.pl>
2026-04-17 23:53:47 +0200

    Logo on navbar moves u to home page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,13 +6,15 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className='navbar-logo'>
-        <img src={godlo} alt="Godło" className="navbar-godlo" />
-        <div className="navbar-brand">
-          <span className="logo-name"><span className="logo-m">m</span>Poparcie</span>
-          <span className="logo-slogan">Głosujesz i decydujesz</span>
+      <Link to="/" className="navbar-logo-link">
+        <div className='navbar-logo'>
+          <img src={godlo} alt="Godło" className="navbar-godlo" />
+          <div className="navbar-brand">
+            <span className="logo-name"><span className="logo-m">m</span>Poparcie</span>
+            <span className="logo-slogan">Głosujesz i decydujesz</span>
+          </div>
         </div>
-      </div>
+      </Link>
 
       <ul className="navbar-links">
         <li><Link to="/register" className="nav-btn-secondary">Rejestracja</Link></li>
