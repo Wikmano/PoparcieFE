4ff044e1d8a6fd6af67e4886e6cb4b22e0b3fd08
Vladyslav Solovei <vladislav17052006@gmail.com>
2026-04-19 19:28:30 +0200

    common names moved to Constants.js

---
+++ src/infrastructure/Constants.js
@@ -0,0 +1,7 @@
+
+export class InfrastructureConstants {
+    static AuthorizationHeader = 'authorization';
+    static BearerHeader = 'bearer';
+    static Token = 'token';
+    static Username = 'username';
+}
