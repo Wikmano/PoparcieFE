c17aa803affca4c143535fee157eb114bc4db194
Krol-Kacper <279415@student.pwr.edu.pl>
2026-04-25 16:00:13 +0200

    ai slop

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     allowedHosts: ['mpoparcie.kackrol.ovh'],
+    hmr: {
+      host: 'mpoparcie.kackrol.ovh',
+      protocol: 'wss',
+      clientPort: 443,
+    },
   },
 });
