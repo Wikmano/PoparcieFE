78390a312bdba4177001285898f67457d6c00c49
Krol-Kacper <279415@student.pwr.edu.pl>
2026-05-11 00:38:38 +0200

    fix endpoint

--- src/services/zkpService.js
+++ src/services/zkpService.js
@@ -2,15 +2,11 @@ import axios from 'axios';
 import { BASE_API_URL } from '../AppConfig.js';
 
 const zkpApi = axios.create({
-  baseURL: BASE_API_URL + 'zkp/',
+  baseURL: BASE_API_URL + 'vote/',
   withCredentials: true,
 });
 
 export const zkpService = {
-  getPath: async (commitment) => {
-    const response = await zkpApi.get('getpath', { params: { commitment } });
-    return response.data;
-  },
   sign: async (proofData) => {
     const response = await zkpApi.post('sign', proofData);
     return response.data;
